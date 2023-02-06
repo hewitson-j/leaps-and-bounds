7c1e26e61c4aa301831fa81889f1579bb2e46767
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-02-05 18:47:47 -0700

    Initial commit, created header.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+import Logo from "./images/Logo.jpg";
+
+function Header() {
+  return (
+    <>
+      <img src={Logo} alt="Leaps and Bounds Logo"></img>
+    </>
+  );
+}
+
+export default Header;
