a60fc0c6a0bbf4fb8daac492c00bd36336b884fc
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-02-05 21:20:52 -0700

    Added contact info section.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import ContactInfo from "./components/ContactInfo";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ContactInfo />
       <Navbar />
     </div>
   );
---
+++ src/components/ContactInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ContactInfo.css";
+
+function ContactInfo() {
+  return (
+    <div className="contact-info-header">
+      <h4>melonro143@yahoo.com</h4>
+      <h4>719-439-0566</h4>
+    </div>
+  );
+}
+
+export default ContactInfo;
